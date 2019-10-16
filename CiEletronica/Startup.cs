78eb792c23dc2d5370631cc22062081a8c484a88
Quemuel Leal <kprofissional@gmail.com>
2019-10-15 21:42:30 -0300

    Put in server github

---
+++ CiEletronica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CiEletronica.Startup))]
+namespace CiEletronica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
